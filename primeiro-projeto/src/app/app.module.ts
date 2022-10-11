bc3a6cc42f3d76ac6bd1cfc7343f175f294af40f
Jonatan Fragoso <jonatanfragoso@gmail.com>
2022-10-11 17:06:13 -0500

    subindo arquivos aula 5 (módulos)

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { CursosModule } from './cursos/cursos.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 
@@ -12,7 +13,8 @@ import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
