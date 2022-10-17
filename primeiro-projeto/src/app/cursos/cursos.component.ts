15b4a90205d6d0ccf66802c777afaff0a1705932
Jonatan Fragoso <jonatanfragoso@gmail.com>
2022-10-17 16:06:12 -0500

    Aula 7 - injeção de dependências.

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,21 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
+
 export class CursosComponent implements OnInit {
 
   nomePortal: string;
 
-  cursos: string[] = ['Java', 'Angular' , 'Spring']
+  cursos: string[];
 
-  constructor() {
+  constructor(
+    private cursosService: CursosService
+    ) {
     this.nomePortal = 'http://loiane.training';
+
+    this.cursos = this.cursosService.getCursos();
   }
 
+
+
   ngOnInit(): void {
+
   }
 
 }
