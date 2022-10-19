d88d7b6c210caa2754ed5f45f1509b4adf796604
Jonatan Fragoso <jonatanfragoso@gmail.com>
2022-10-19 16:00:57 -0500

    aula 11 - event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
   styleUrls: ['./data-binding.component.css']
+
 })
 export class DataBindingComponent implements OnInit {
 
@@ -12,6 +13,12 @@ export class DataBindingComponent implements OnInit {
   }
   url: string = 'http://loiane.com';
 
+  valorAtual: string = '';
+
+  valorSalvo: string = '';
+
+  isMouseOver: boolean = false;
+
   cursoAngular: boolean = true;
   urlImagem = "https://img.freepik.com/fotos-gratis/caminho-no-meio-de-edificios-sob-um-ceu-escuro-no-japao_181624-43078.jpg?w=900&t=st=1666122763~exp=1666123363~hmac=4dff9a43f1efd0d6f8b3b41f958c262557166538ffb75dee7a37a879333b17cb"
 
@@ -19,7 +26,24 @@ export class DataBindingComponent implements OnInit {
     return true;
   }
 
+  botaoClicado(){
+    alert('Botão clicado!');
 
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    console.log();
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+
+  }
+
+  salvarValor(valor: string){
+    this.valorSalvo = valor;
+  }
+
+  OnMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
 
   constructor() { }
 
