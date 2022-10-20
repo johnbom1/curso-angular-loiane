634b3b828f78debf4cf800baae2f97eb73484592
Jonatan Fragoso <jonatanfragoso@gmail.com>
2022-10-20 16:18:52 -0500

    Aula 13 - rausando componentes input properties

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -22,7 +22,7 @@ export class DataBindingComponent implements OnInit {
   cursoAngular: boolean = true;
   urlImagem = "https://img.freepik.com/fotos-gratis/caminho-no-meio-de-edificios-sob-um-ceu-escuro-no-japao_181624-43078.jpg?w=900&t=st=1666122763~exp=1666123363~hmac=4dff9a43f1efd0d6f8b3b41f958c262557166538ffb75dee7a37a879333b17cb";
 
-
+  nomeDoCurso: string = 'Angular';
 
 
   getCurtirCurso(){
