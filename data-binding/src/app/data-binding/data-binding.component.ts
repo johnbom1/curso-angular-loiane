fbb2565e10d054704ea378df929e43ace1badcb4
Jonatan Fragoso <jonatanfragoso@gmail.com>
2022-10-20 02:17:55 -0500

    aulta 12 - two-way data binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -20,7 +20,10 @@ export class DataBindingComponent implements OnInit {
   isMouseOver: boolean = false;
 
   cursoAngular: boolean = true;
-  urlImagem = "https://img.freepik.com/fotos-gratis/caminho-no-meio-de-edificios-sob-um-ceu-escuro-no-japao_181624-43078.jpg?w=900&t=st=1666122763~exp=1666123363~hmac=4dff9a43f1efd0d6f8b3b41f958c262557166538ffb75dee7a37a879333b17cb"
+  urlImagem = "https://img.freepik.com/fotos-gratis/caminho-no-meio-de-edificios-sob-um-ceu-escuro-no-japao_181624-43078.jpg?w=900&t=st=1666122763~exp=1666123363~hmac=4dff9a43f1efd0d6f8b3b41f958c262557166538ffb75dee7a37a879333b17cb";
+
+
+
 
   getCurtirCurso(){
     return true;
